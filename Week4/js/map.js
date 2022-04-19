f5bff16ecd7792d98cd8bae1e43da07249ea0735
Cynthia Gong <cynthiagong@g.ucla.edu>
2022-04-19 15:12:47 -0700

    map updates

--- Week4/js/map.js
+++ Week4/js/map.js
@@ -5,7 +5,7 @@ let lon = 0;
 let zl = 1;
 
 // path to csv data
-let path = "data/crime.csv";
+let path = "data/arrest.csv";
 
 let markers = L.featureGroup();
 
@@ -20,10 +20,14 @@ $( document ).ready(function() {
 function createMap(lat,lon,zl){
 	map = L.map('map').setView([lat,lon], zl);
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
+
+function flyToIndex(lat, lon){
+	map.flyTo([lat,lon],12)
+};
 
 // function to read csv data
 function readCSV(path){
@@ -42,12 +46,24 @@ function readCSV(path){
 
 function mapCSV(data){
 	
+	// circle options
+	let circleOptions = {
+		radius: 5,
+		weight: 1,
+		color: 'white',
+		fillColor: 'red',
+		fillOpacity: 1
+	}
+
 	// loop through each entry
 	data.data.forEach(function(item,index){
 		// create marker
-		let marker = L.marker([item.latitude,item.longitude])
+		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+		.on('mouseover', function(){
+			this.bindPopup(`${item.date}<br>${item.desc}`).openPopup()
+		})
 
-		// add marker to featuregroup
+		// add marker to featuregroup		
 		markers.addLayer(marker)
 	})
 
